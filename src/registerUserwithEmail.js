ddb47bde10080c149c0862083e36fbb403c2961e
julianm00 <juli.mai2000@gmail.com>
2022-05-06 12:46:15 +0200

    Unnötige Files gelöscht

--- src/registerUserwithEmail.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async function registerUserWithEmail(data) {
-  try {
-    const { email } = data;
-    console.log("register User with Email", email);
-  } catch (err) {
-    console.log("error with registerUserWithEmail", err);
-    return err;
-  }
-};
